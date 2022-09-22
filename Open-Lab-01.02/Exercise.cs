9b716c9dcf1f1fd6316c06be2bddcb1996a50605
TomBuk4 <104893605+TomBuk4@users.noreply.github.com>
2022-09-22 07:56:16 +0200

    Update Exercise.cs

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -5,6 +5,8 @@ namespace Open_Lab_01._02
     public class Exercise
     {
         public int Sum(int a, int b)
+           
+            return a + b
         {
             throw new NotImplementedException();
         }
